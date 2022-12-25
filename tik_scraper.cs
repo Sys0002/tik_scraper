02cea099334dd8fa0a6a0639c7490d2a7f49831e
Chris <69528561+Sys0002@users.noreply.github.com>
2022-12-25 08:39:19 +0100

    Update tik_scraper.cs

--- tik_scraper.cs
+++ tik_scraper.cs
@@ -1,27 +1,34 @@
 using HtmlAgilityPack;
 using System.Net;
+using System;
 
 namespace Tik_Shit {
   class Program {
     static void Main(string[] args) {
-      var client = new WebClient();
-      var html = client.DownloadString("https://www.tiktok.com/@username");
+      try {
+        var client = new WebClient();
+        var html = client.DownloadString("https://www.tiktok.com/@username");
 
-      var document = new HtmlDocument();
-      document.LoadHtml(html);
+        var document = new HtmlDocument();
+        document.LoadHtml(html);
 
-      var name_el = document.DocumentNode.SelectSingleNode("//h1[contains(@class, 'username')]");
-      var name = name_el.InnerText;
+        var name_el = document.DocumentNode.SelectSingleNode("//h1[contains(@class, 'username')]");
+        var name = name_el.InnerText;
 
-      var bio_el = document.DocumentNode.SelectSingleNode("//h2[contains(@class, 'bio')]");
-      var bio = bio_el.InnerText;
+        var bio_el = document.DocumentNode.SelectSingleNode("//h2[contains(@class, 'bio')]");
+        var bio = bio_el.InnerText;
 
-      var followers_el = document.DocumentNode.SelectSingleNode("//span[contains(@class, 'followers')]");
-      var followers = followers_el.InnerText;
+        var followers_el = document.DocumentNode.SelectSingleNode("//span[contains(@class, 'followers')]");
+        var followers = followers_el.InnerText;
 
-      Console.WriteLine("Name: " + name);
-      Console.WriteLine("Bio: " + bio);
-      Console.WriteLine("Followers: " + followers);
+        Console.WriteLine("Name: " + name);
+        Console.WriteLine("Bio: " + bio);
+        Console.WriteLine("Followers: " + followers);
+      } catch (WebException ex) {
+        Console.WriteLine("Error accessing TikTok website: " + ex.Message);
+      } catch (NullReferenceException ex) {
+        Console.WriteLine("Error parsing TikTok profile info: " + ex.Message);
+      }
     }
   }
 }
